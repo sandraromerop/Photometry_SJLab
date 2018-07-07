8f69025e1655c53cc0bc4b8ed1ac392c73b3bf88
LARS-HP\Sandra <sandrarp,er>
2018-07-07 15:18:40 +0100

    Cued_reward script suitable for new plotting

--- Protocols/CuedReward/CuedReward.m
+++ Protocols/CuedReward/CuedReward.m
@@ -77,6 +77,8 @@ if S.GUI.eZTrials
 end
 S.NumTrialTypes=length(unique(TrialSequence));
 trialNamesPlot= S.TrialsNames;trialNamesPlot = trialNamesPlot(unique(TrialSequence));
+S.TrialOutcomes = cell(1,S.NumTrialTypes);
+
 % Select to plot only those 
 FigLick=Online_LickPlot('ini',TrialSequence,S.TrialsMatrix,...
     trialNamesPlot ,S.Names.Phase{S.GUI.Phase});
@@ -120,9 +122,7 @@ for currentTrial = 1:S.GUI.MaxTrials
 	S.Delay             =	S.TrialsMatrix(TrialSequence(currentTrial),4)+(S.GUI.DelayIncrement*(currentTrial-1));
 	S.Valve             =	S.TrialsMatrix(TrialSequence(currentTrial),5);
 	S.Outcome           =   S.TrialsMatrix(TrialSequence(currentTrial),6);
-    
-    
-    
+
     S.OptoStimCue       =   S.TrialsMatrix(TrialSequence(currentTrial),8);
     S.OptoStimCueDelay  =   S.TrialsMatrix(TrialSequence(currentTrial),9);
     S.OptoStimRwd       =   S.TrialsMatrix(TrialSequence(currentTrial),10);
@@ -142,7 +142,7 @@ try
     ProgramPulsePalParam(1, 'PulseTrainDelay', S.GUI.PulseTrainDelay);
     ProgramPulsePalParam(1, 'InterPhaseInterval', S.GUI.InterPhaseInterval);
     ProgramPulsePalParam(1, 'PulseTrainDuration', S.GUI.PulseTrainDuration);
-    ProgramPulsePalParam(1,12, S.GUI.LinkedToTriggerCH1);
+    ProgramPulsePalParam(1, 12, S.GUI.LinkedToTriggerCH1);
     ProgramPulsePalParam(1, 13, S.GUI.LinkedToTriggerCH2);
 end
 %% Assemble State matrix
@@ -228,8 +228,11 @@ end
 trialNamesPlot= S.TrialsNames;trialNamesPlot = trialNamesPlot(unique(TrialSequence));
 trialTypePlot = find(strcmp(trialNamesPlot,S.TrialsNames(TrialSequence(currentTrial))));
 
-[currentOutcome, currentLickEvents]=Online_LickEvents(S.TrialsMatrix,currentTrial,TrialSequence(currentTrial),S.Names.StateToZero{S.GUI.StateToZero});
+[currentOutcome, currentLickEvents,outcomeId]=Online_LickEvents(S.TrialsMatrix,currentTrial,TrialSequence(currentTrial),...
+    S.Names.StateToZero{S.GUI.StateToZero});
+S.TrialOutcomes{find(unique(TrialSequence)==TrialSequence(currentTrial))}  = [S.TrialOutcomes{find(unique(TrialSequence)==TrialSequence(currentTrial))}   outcomeId];
 FigLick=Online_LickPlot('update',trialNamesPlot,[],trialNamesPlot,[],FigLick,currentTrial,currentOutcome,trialTypePlot,currentLickEvents);
+tic
 if S.GUI.Photometry
      [currentNidaq470, nidaqRaw]=Online_NidaqDemod(PhotoData(:,1),nidaq.LED1,...
         S.GUI.LED1_Freq,S.GUI.LED1_Amp,S.Names.StateToZero{S.GUI.StateToZero},currentTrial);
@@ -239,13 +242,15 @@ if S.GUI.Photometry
     else
         currentNidaq405=[0 0];
     end
-    FigNidaq=Online_NidaqPlot('update',trialNamesPlot,[],FigNidaq,currentNidaq470,currentNidaq405,nidaqRaw,trialTypePlot);
+    FigNidaq=Online_NidaqPlot('update',trialNamesPlot,[],FigNidaq,currentNidaq470,...
+        currentNidaq405,nidaqRaw,trialTypePlot);
     %% 
     if S.GUI.DbleFibers == 1
         [currentNidaq470b,nidaqRawb]=Online_NidaqDemod(Photo2Data(:,1),nidaq.LED2,S.GUI.LED1b_Freq,S.GUI.LED1b_Amp,S.Names.StateToZero{S.GUI.StateToZero},currentTrial);
         FigNidaqB=Online_NidaqPlot('update',[],[],FigNidaqB,currentNidaq470b,currentNidaq405,nidaqRawb,TrialSequence(currentTrial));
     end
 end
+toc
 
 if S.GUI.Wheel
     FigWheel=Online_WheelPlot('update',FigWheel,WheelData,S.Names.StateToZero{S.GUI.StateToZero},currentTrial,currentLickEvents);
