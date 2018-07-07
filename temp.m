d2b7abcc117fc7bc7bb5f8edaa769711ebf79f2d
SWCDeskptop_Sandra <sandrarp,er>
2018-07-07 15:48:31 +0100

    Trial Settings for new task

---
+++ temp.m
@@ -0,0 +1,48 @@
+TrialSettings.ProbA =0.25
+TrialSettings.ProbAOmission =0.25
+TrialSettings.ProbB=0.25
+TrialSettings.ProbBOmission=0
+TrialSettings.ProbC=0.25
+TrialSettings.ProbCOmission=0
+TrialSettings.ProbUncuedRwd=0
+TrialSettings.ProbUncuedOmission=0
+TrialSettings.ProbAOptoCue=0
+TrialSettings.ProbBOptoCue=0
+TrialSettings.ProbCOptoCue=0
+TrialSettings.ProbAOptoOutcome=0
+TrialSettings.ProbBOptoOutcome=0
+TrialSettings.ProbCOptoOutcome=0
+TrialSettings.OrderA=[]
+TrialSettings.OrderAOmission=[]
+TrialSettings.OrderB=[]
+TrialSettings.OrderBOmission=[]
+TrialSettings.OrderC=[]
+TrialSettings.OrderCOmission=[]
+TrialSettings.OrderUncuedRwd=[]
+TrialSettings.OrderUncuedOmission=[]
+TrialSettings.OrderAOptoCue=[]
+TrialSettings.OrderBOptoCue=[]
+TrialSettings.OrderCOptoCue=[]
+TrialSettings.OrderAOptoOutcome=[]
+TrialSettings.OrderBOptoOutcome=[]
+TrialSettings.OrderCOptoOutcome=[]
+
+
+
+TrialSettings.DelayA = .5;
+TrialSettings.DelayB    = .5;
+TrialSettings.DelayC= .5;
+TrialSettings.RewardValveA=1;
+TrialSettings.RewardValveB=1;
+TrialSettings.RewardValveC=1;
+TrialSettings.RewardA=8;
+TrialSettings.RewardB=5;
+TrialSettings.RewardC=2;
+
+TrialSettings.FreqA=  4000;
+TrialSettings.FreqB=  10500;
+TrialSettings.FreqC = 22600; 
+
+
+save(['C:\Users\Lars\Documents\Photometry_SJLab\TrialSettings_StateUncertainty.mat'],'TrialSettings')
+save(['C:\Users\Lars\Documents\Photometry_SJLab\TrialSettings_StateUncertainty_Default.mat'],'TrialSettings')
