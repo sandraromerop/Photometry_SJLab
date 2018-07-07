65661df307f5fce0b4cc6e6d6ffc714556ec34e2
LARS-HP\Sandra <sandrarp,er>
2018-07-07 15:12:11 +0100

    self-stimulation analysis

---
+++ Analysis/Analysis_SelfStim/AP_PlotSelfStim.m
@@ -0,0 +1,42 @@
+function AP_PlotSelfStim(Analysis)
+
+% ---------------------------------------------------------------- Plots
+% Responses in time
+timeFromPoke = Analysis.DefaultParam.timeFromPoke;
+protocol =Analysis.DefaultParam.protocol;
+subject =Analysis.DefaultParam.subject;
+date =Analysis.DefaultParam.date;
+session =Analysis.DefaultParam.session;
+startPoke = Analysis.startPoke ;
+
+startId = find(Analysis.ResponseTimeFromPoke1/20>=0);startId=startId(1);
+endId = find(Analysis.ResponseTimeFromPoke1/20<=timeFromPoke);endId=endId(end);
+
+temp = Analysis.ResponseTimeFromPoke1/60;temp = temp(startId:endId);
+startBin = find((Analysis.Bins-startPoke)/20>=0);startBin=startBin(1);
+endBin = find((Analysis.Bins-startPoke)/20<=timeFromPoke);endBin=endBin(end);
+
+
+figure('units','normalized','position',[.2 .2 .5 .5])
+subplot(2,2,1)
+plot(temp(find(Analysis.Logicals(startId:endId,1)==1)),ones(1,length(find(Analysis.Logicals(startId:endId,1)==1))),'o','Color','blue');
+hold on
+plot(temp(find(Analysis.Logicals(startId:endId,2)==1)),2*ones(1,length(find(Analysis.Logicals(startId:endId,2)==1))),'o','Color','red');
+ax=gca;ax.YLim=[0 3];
+xlabel('Time from  First Poke  (min)');ylabel('Trial Type');
+ax.YTick = [ 1 2];ax.YTickLabels = {'Laser','Sham Laser'};
+title([protocol ' ' subject  ' ' strrep(date ,'_',' ') ' -Session ' num2str(session )]);
+
+% Percentages
+subplot(2,2,2)
+bar(1,Analysis.PercentagesFromPoke1(1),'FaceColor','blue','EdgeColor','blue');hold on
+bar(2,Analysis.PercentagesFromPoke1(2),'FaceColor','red','EdgeColor','red');
+ylabel('Percentage of Trials from First Poke')
+ax=gca;ax.YLim=[0 100];
+ax.XTick = [ 1 2];ax.XTickLabels = {'Laser','Sham Laser'};
+
+subplot(2,2,3)
+plot((Analysis.Bins(startBin:endBin-1)-Analysis.startPoke )/60, Analysis.PokeRates(1,startBin:endBin-1),'Color','blue');hold on
+plot((Analysis.Bins(startBin:endBin-1)-Analysis.startPoke )/60, Analysis.PokeRates(2,startBin:endBin-1),'Color','red');
+xlabel('Time from  First Poke (min)');ylabel('Poke Rate (Hz)');
+legend('Laser','Sham Laser')
---
+++ Analysis/Analysis_SelfStim/selfStim_launcher.m
@@ -0,0 +1,83 @@
+% Code analysis Self stimulation
+close all
+generalDir = 'F:\Photometry_SJLab\Data\';
+subject ='Dummy Subject' ;
+session = 10;
+date = 'Jun19_2018';
+protocol = 'Self-Stimulation';
+n=1;
+clear Analysis
+
+DefaultParam.binRate =1;
+DefaultParam.timeFromPoke  = 20 ; %minutes
+DefaultParam.generalDir = generalDir;
+DefaultParam.protocol  = protocol ;
+DefaultParam.subject = subject ;
+DefaultParam.date = date;
+DefaultParam.session = session;
+
+load([ generalDir  subject  '\' protocol '\Session Data\' ...
+    subject   '_' strrep(protocol,'-','') '_' date   '_Session' num2str(session) '.mat'])
+
+
+%% Get rate of poking across entire session for each port
+
+
+% Do logic depending on if it was rewarded or not
+binRate = DefaultParam.binRate ;
+timeFromPoke = DefaultParam.timeFromPoke ;
+Analysis.DefaultParam=DefaultParam;
+nTrials = SessionData.nTrials;
+
+logicals = zeros(nTrials,2);trialTypes = cell(nTrials,1);
+trialStarts = SessionData.TrialStartTimestamp;
+
+for n=nTrials:-1:1
+    states(n) = SessionData.RawEvents.Trial{1,n}.States;
+    if ~isnan(SessionData.RawEvents.Trial{1,n}.States.Laser(1))
+        logicals(n,1) = 1;trialTypes{n} = 'Laser';
+        trialPoke = n;
+    else
+        logicals(n,2) = 1;trialTypes{n} = 'ShamLaser';
+    end
+    responseTime(n) = SessionData.RawEvents.Trial{1,n}.States.WaitForResponse(end);
+    responseTimeStamp(n)= trialStarts(n)+responseTime(n) ;
+end
+startPoke = trialStarts(trialPoke);
+Analysis.startPoke = startPoke;
+Analysis.Logicals = logicals;
+Analysis.TrialTypes = trialTypes;
+Analysis.TrialStarts = trialStarts(:);
+Analysis.ResponseTimeStamp = responseTimeStamp(:);
+Analysis.ResponseTimePerTrial = responseTime(:);% plot
+Analysis.ResponseTimeFromStart = responseTimeStamp -trialStarts(1);% plot
+Analysis.ResponseTimeFromPoke1 = responseTimeStamp -startPoke;% plot
+startId = find(Analysis.ResponseTimeFromPoke1/20>=0);startId=startId(1);
+endId = find(Analysis.ResponseTimeFromPoke1/20<=timeFromPoke);endId=endId(end);
+
+Analysis.PercentagesFromStart = sum(logicals)./(length(logicals))*100;
+Analysis.PercentagesFromPoke1 = sum(logicals(startId:endId,:))./(length(logicals(startId:endId,:)))*100;
+
+
+% Rate across Session  from first poke time
+Analysis.Bins = Analysis.TrialStarts(1):binRate :Analysis.TrialStarts(end);
+Analysis.PokeRates(1,:) = histcounts(responseTimeStamp(find(logicals(:,1)==1)),...
+   Analysis.Bins)/binRate;
+Analysis.PokeRates(2,:) = histcounts(responseTimeStamp(find(logicals(:,2)==1)),...
+   Analysis.Bins)/binRate;
+
+
+
+Analysis.Properties.Files=[subject  '_' strrep(protocol,'-','') '_' date  '_Session' num2str(session) '.mat'];
+DirAnalysis=[ generalDir  subject  '\' protocol '\Session Data\Analysis\'];
+if ~isdir(DirAnalysis)
+    mkdir(DirAnalysis);
+end
+FileName=[subject  '_' strrep(protocol,'-','') '_' date  '_Session' num2str(session ) '_Analysis.mat'];
+DirFile=[DirAnalysis FileName];
+save(DirFile,'Analysis');
+
+
+AP_PlotSelfStim(Analysis)
+
+
