9216c3fa548d9fd3c5bc671904761c959f3a562b
sandraromerop <sandraromeropinto@gmail.com>
2018-07-31 13:13:03 +0100

    Add files via upload

--- Analysis/AP_PlotSummary_VariableState.m
+++ Analysis/AP_PlotSummary_VariableState.m
@@ -1,7 +1,6 @@
-function AP_PlotSummary_AllSignals(Analysis,DefaultParam )
+function AP_PlotSummary_VariableState(Analysis,DefaultParam )
 for stateNb=1:size(Analysis.AllData.Photo_470.DFF,1)
-figure('units','normalized','position',[.1 .1 .7 .7])
-
+figure('units','normalized','position',[.1 .1 .4 .7])
 idFilt=[];nbOfTrialTypes= Analysis.Properties.nbOfTrialTypes;
 for i=1:nbOfTrialTypes
      [thisFilter] = getFilter(Analysis,i);   
@@ -12,36 +11,73 @@ trialTypes = idTrials;
 trialTypes(find(all(idFilt==0,1)))=[];
 trialTypes(trialTypes>nbOfTrialTypes)=[];
 
-minY = {[] [] []};p90 = {[] [] []};p10 = {[] [] []};maxY ={[] [] []};
+minY = {[0] [0] [0]};
+p90 = {[0] [0] [0]};
+p10 = {[0] [0] [0]};
+maxY ={[0] [0] [0]};
+
+varType = [];
 for tT = trialTypes
     [thisFilter] = getFilter(Analysis,tT);    
+    outcomeTimes = squeeze(Analysis.AllData.OutcomeTime(2,thisFilter,:));
+    if outcomeTimes(2)-outcomeTimes(1)~=0
+        varType = [varType  tT]; 
+    end
+end
+
+for tT = varType
+    [thisFilter] = getFilter(Analysis,tT);    
     tempPhoto_470 = squeeze( Analysis.AllData.Photo_470.DFF(stateNb,thisFilter,1:end));
-    maxY{1}= [maxY{1} max(nanmean(tempPhoto_470(:,1:end),1)+ nanstd((tempPhoto_470(:,1:end)),[],1)./(sqrt(size(tempPhoto_470,1))))];
-    minY{1}= [minY{1} min(nanmean(tempPhoto_470(:,1:end),1)+ nanstd((tempPhoto_470(:,1:end)),[],1)./(sqrt(size(tempPhoto_470,1))))];
-    p90{1} = [p90{1} prctile(tempPhoto_470,90)];
-    p10{1} = [p10{1} prctile(tempPhoto_470,10)];
-    
     tempWheel =  diff( squeeze(Analysis.AllData.Wheel.Distance(stateNb,thisFilter,:)),[],2);
-    maxY{2}= [maxY{2} max(nanmean(tempWheel(:,1:end),1)+ nanstd((tempWheel(:,1:end)),[],1)./(sqrt(size(tempWheel,1))))];
-    minY{2}= [minY{2} min(nanmean(tempWheel(:,1:end),1)+ nanstd((tempWheel(:,1:end)),[],1)./(sqrt(size(tempWheel,1))))];
-    p90{2} = [p90{2} prctile(tempWheel,90)];
-    p10{2} = [p10{2} prctile(tempWheel,10)];
-    
     tempLick = squeeze( Analysis.AllData.Licks.Rate(stateNb,thisFilter,:));
-    maxY{3}= [maxY{3} max(nanmean(tempLick(:,1:end),1)+ nanstd((tempLick(:,1:end)),[],1)./(sqrt(size(tempLick,1))))];
-    minY{3}= [minY{3} min(nanmean(tempLick(:,1:end),1)+ nanstd((tempLick(:,1:end)),[],1)./(sqrt(size(tempLick,1))))];
-    p90{3} = [p90{3} prctile(tempLick,90)];
-    p10{3} = [p10{3} prctile(tempLick,10)];
+
+    outcomeTimes = squeeze(Analysis.AllData.OutcomeTime(2,thisFilter,:));
+    uniqueTimes = unique(outcomeTimes(:,1));
+    
+    for uT=1:length(uniqueTimes)
+        idT = find(outcomeTimes(:,1) == uniqueTimes(uT));
+        meanPhoto = nanmean(tempPhoto_470(idT,:),1);stdPhoto =  nanstd(tempPhoto_470(idT,:),[],1);
+        meanWheel = nanmean(tempWheel(idT,:),1); stdWheel =  nanstd(tempWheel(idT,:),[],1);
+        meanLick = nanmean(tempLick(idT,:),1);stdLick =   nanstd(tempLick(idT,:),[],1);
+
+        maxY{1}= [maxY{1} max(meanPhoto+ stdPhoto./(sqrt(length(idT))))];
+        minY{1}= [minY{1} min(meanPhoto- stdPhoto./(sqrt(length(idT))))];
+        p90{1} = [p90{1} prctile(tempPhoto_470(idT,:),90)];
+        p10{1} = [p10{1} prctile(tempPhoto_470(idT,:),10)];
+
+        maxY{2}= [maxY{2} max(meanWheel+ stdWheel./(sqrt(length(idT))))];
+        minY{2}= [minY{2} min(meanWheel- stdWheel./(sqrt(length(idT))))];
+        p90{2} = [p90{2} prctile(tempWheel(idT,:),90)];
+        p10{2} = [p10{2} prctile(tempWheel(idT,:),10)];
+
+        
+        maxY{3}= [maxY{3} max(meanLick+ stdLick./(sqrt(length(idT))))];
+        minY{3}= [minY{3} min(meanLick- stdLick./(sqrt(length(idT))))];
+        p90{3} = [p90{3} prctile(tempLick(idT,:),90)];
+        p10{3} = [p10{3} prctile(tempLick(idT,:),10)];
+        
+        
+    end
     
 end
-maxY{1}(isnan(maxY{1}))=[];maxY{1}=max(maxY{1});maxY{2}(isnan(maxY{2}))=[];maxY{2}=max(maxY{2});maxY{3}(isnan(maxY{3}))=[];maxY{3}=max(maxY{3});
-minY{1}(isnan(minY{1}))=[];minY{1}=min(minY{1});minY{2}(isnan(minY{2}))=[];minY{2}=min(minY{2});minY{3}(isnan(minY{3}))=[];minY{3}=min(minY{3});
-p90{1}(isnan(p90{1}))=[];p90{1}=max(p90{1});p90{2}(isnan(p90{2}))=[];p90{2}=max(p90{2});p90{3}(isnan(p90{3}))=[];p90{3}=max(p90{3});
-p10{1}(isnan(p10{1}))=[];p10{1}=min(p10{1});p10{2}(isnan(p10{2}))=[];p10{2}=min(p10{2});p10{3}(isnan(p10{3}))=[];p10{3}=min(p10{3});
+maxY{1}(isnan(maxY{1}))=[];maxY{1}=max(maxY{1});maxY{2}(isnan(maxY{2}))=[];maxY{2}=max(maxY{2});
+maxY{3}(isnan(maxY{3}))=[];maxY{3}=max(maxY{3});minY{1}(isnan(minY{1}))=[];minY{1}=min(minY{1});
+minY{2}(isnan(minY{2}))=[];minY{2}=min(minY{2});minY{3}(isnan(minY{3}))=[];minY{3}=min(minY{3});
+p90{1}(isnan(p90{1}))=[];p90{1}=max(p90{1});p90{2}(isnan(p90{2}))=[];p90{2}=max(p90{2});
+p90{3}(isnan(p90{3}))=[];p90{3}=max(p90{3});p10{1}(isnan(p10{1}))=[];p10{1}=min(p10{1});
+p10{2}(isnan(p10{2}))=[];p10{2}=min(p10{2});p10{3}(isnan(p10{3}))=[];p10{3}=min(p10{3});
 
-matIP =reshape( 1:(6*length(trialTypes)),length(trialTypes),6)';ip=1;
-if size(matIP,1)==1;    matIP=matIP(:);end
-for tT = trialTypes
+matIP =reshape( 1:(6*length(varType)),length(varType),6)';
+if size(matIP,1)==1
+    matIP=matIP(:);
+end
+ip=1;
+
+
+
+
+
+for tT = varType
    
     if ~isempty(strfind( Analysis.Properties.TrialNames{tT},'Opto'))
         if ~isempty(strfind( Analysis.Properties.TrialNames{tT},'OptoStim at Cue'))
@@ -76,31 +112,52 @@ for tT = trialTypes
     tempPhoto_470 = cat(1,tempPhoto_470,nan.*ones(1,size(tempPhoto_470,2)));
     tempWheel = cat(1,tempWheel,nan.*ones(1,size(tempWheel,2)));
     tempLick = cat(1,tempLick,nan.*ones(1,size(tempLick,2)));
+
+    
     trialsNb =find(thisFilter==1);
     tempTime = Analysis.AllData.Photo_470.Time(stateNb,thisFilter,:);
     
-    timesD = [] ; blocksPhoto = [];
+    
+    blocksPhoto = []; meanPhoto = [];stdPhoto = [];
+    blocksWheel = []; meanWheel = [];stdWheel = [];
+    blocksLick = []; meanLick = [];stdLick = [];
+    timesD = [] ; 
     for uT=1:length(uniqueTimes)
         idT = find(outcomeTimes(:,1) == uniqueTimes(uT));
-        timesD = cat(2,timesD, squeeze( tempTime(1,idT(1),1:end)));
+        timeD = squeeze( tempTime(1,idT(1),1:end));
+        timeV = linspace(timeD(1),timeD(end),length(timeD)-1);
+        timesD = cat(2,timesD,timeD);
+        
         blocksPhoto = cat(1,blocksPhoto,tempPhoto_470(idT,:)); 
+        meanPhoto = cat(1,meanPhoto,nanmean(tempPhoto_470(idT,:),1));stdPhoto = cat(1,stdPhoto,nanstd(tempPhoto_470(idT,:),[],1));
+        
+        blocksWheel = cat(1,blocksWheel,tempWheel(idT,:)); 
+        meanWheel = cat(1,meanWheel,nanmean(tempWheel(idT,:),1)); stdWheel = cat(1,stdWheel,nanstd(tempWheel(idT,:),[],1));
+        
+        blocksLick = cat(1,blocksLick,tempLick(idT,:)); 
+        meanLick = cat(1,meanLick,nanmean(tempLick(idT,:),1));stdLick = cat(1,stdLick,nanstd(tempLick(idT,:),[],1));
     end
 
-    newTime = min(timesD(:)):.05:max(timesD(:));
-    blocksAlligned = zeros(size(blocksPhoto,1),length(newTime));ii=1;
+  newTime = min(timesD(:)):.05:max(timesD(:));
+  id0 = find(timesD(:,1)>=0); blocksAlligned = zeros(size(blocksPhoto,1),length(newTime));ii=1;
+  meanPhotoAlligned=[];stdPhotoAlligned=[];
+  for uT=1:length(uniqueTimes)
+      idT = find(outcomeTimes(:,1) == uniqueTimes(uT));
+      timeD = squeeze( tempTime(1,idT(1),1:end));
+      temp = tempPhoto_470(idT,:);
+      interpTemp =  interp1(timeD,temp',newTime);
+      blocksAlligned(ii:ii+length(idT)-1,1:length(interpTemp)) = interpTemp';
+      meanPhotoAlligned(uT,:) = nanmean(interpTemp',find(size(interpTemp')==length(idT)));
+      stdPhotoAlligned(uT,:) = nanstd(interpTemp',[],find(size(interpTemp')==length(idT)));
+      ii=ii+length(idT);
+  end
+  
+    cc= cbrewer('seq','GnBu',length(uniqueTimes)).*.9;
+    subplot(6,length(varType),matIP(1,ip));ii=1; 
     for uT=1:length(uniqueTimes)
-        idT = find(outcomeTimes(:,1) == uniqueTimes(uT));
-        temp = tempPhoto_470(idT,:);
-        interpTemp =  interp1(squeeze( tempTime(1,idT(1),1:end)),temp',newTime);
-        blocksAlligned(ii:ii+length(idT)-1,1:length(interpTemp)) = interpTemp';
-        ii=ii+length(idT);
+        shadedErrorBar(newTime,meanPhotoAlligned(uT,:),...
+            stdPhotoAlligned(uT,:)./(sqrt(length( find(outcomeTimes(:,1) == uniqueTimes(uT))))),cc(uT,:));hold on
     end
-    idT=find(thisFilter==1);idT=idT(1);
-    timeD = squeeze( Analysis.AllData.Photo_470.Time(stateNb,idT,1:end));
-    timeV = linspace(timeD(1),timeD(end),length(timeD)-1);
-    subplot(6,length(trialTypes),matIP(1,ip)); 
-    shadedErrorBar(newTime,nanmean(blocksAlligned(:,1:end),1),...
-        nanstd((blocksAlligned(:,1:end)),[],1)./(sqrt(size(blocksAlligned,1))),'-b',1)
     hold on
     plot([0 0],[0 maxY{1}],'Color',[.5 .5 .5]);
     plot(squeeze( Analysis.AllData.CueTime(stateNb,1,:)),[maxY{1} maxY{1}],'Color',[.7 .7 .7],'LineWidth',2,'LineStyle','-');
@@ -112,21 +169,22 @@ for tT = trialTypes
     end
     box off  
     ylabel('DF/F0')
-    if tT==1
-        try
-            title([ {strrep(DefaultParam.FileList(1:end-4),'_',' ');   Analysis.Properties.TrialNames{tT}  }]);          
-        catch
-            title([ {strrep(DefaultParam.FileToOpen(1:end-4),'_',' ');   Analysis.Properties.TrialNames{tT}  }]);          
-        end
-    else
-        title([  Analysis.Properties.TrialNames{tT}  ]);          
+    try
+        title([ {strrep(DefaultParam.FileList(1:end-4),'_',' ');   Analysis.Properties.TrialNames{tT}  }]);          
+    catch
+        title([ {strrep(DefaultParam.FileToOpen (1:end-4),'_',' ');   Analysis.Properties.TrialNames{tT}  }]);          
     end
     ax=gca;ax.XLim=[timeD(1) timeD(end)];
     ax.YLim =[minY{1} maxY{1}+.1];
      
-    subplot(6,length(trialTypes),matIP(2,ip)); 
-    shadedErrorBar(timeV,nanmean(abs(tempWheel(:,1:end)),1),...
-        nanstd(abs(tempWheel(:,1:end)),[],1)./(sqrt(size(tempWheel,1))),'-r',1)
+    subplot(6,length(varType),matIP(2,ip));
+    for uT=1:length(uniqueTimes)
+        idT = find(outcomeTimes(:,1) == uniqueTimes(uT));
+        timeD = squeeze( Analysis.AllData.Photo_470.Time(stateNb,idT(1),1:end));
+        timeV = linspace(timeD(1),timeD(end),length(timeD)-1);
+        shadedErrorBar(timeV,meanWheel(uT,:),...
+            stdWheel(uT,:)./(sqrt(length( find(outcomeTimes(:,1) == uniqueTimes(uT))))),cc(uT,:));hold on
+    end
     hold on
     plot([0 0],[0 maxY{2}],'Color',[.5 .5 .5]);
     plot(squeeze( Analysis.AllData.CueTime(stateNb,1,:)),[maxY{2} maxY{2}].*.75,'Color',[.7 .7 .7],'LineWidth',2,'LineStyle','-');
@@ -141,9 +199,15 @@ for tT = trialTypes
     ax=gca;ax.XLim=[timeD(1) timeD(end)];
     ax.YLim =[minY{2} maxY{2}+.1];
    
-    subplot(6,length(trialTypes),matIP(3,ip))
-    shadedErrorBar(Analysis.AllData.Licks.Bin{1}(2:end) ,nanmean(tempLick,1),...
-        nanstd(tempLick,[],1)./(sqrt(size(tempWheel,1))),'-g',1)
+    
+    
+    subplot(6,length(varType),matIP(3,ip))
+    for uT=1:length(uniqueTimes)
+        idT = find(outcomeTimes(:,1) == uniqueTimes(uT));
+        timeD = squeeze( Analysis.AllData.Photo_470.Time(stateNb,idT(1),1:end));
+        shadedErrorBar(Analysis.AllData.Licks.Bin{1}(2:end),meanLick(uT,:),...
+            stdLick(uT,:)./(sqrt(length( find(outcomeTimes(:,1) == uniqueTimes(uT))))),cc(uT,:));hold on
+    end    
     xlabel('Time (sec)')
     ylabel('Lick Rate (Hz)')
     hold on
@@ -154,11 +218,12 @@ for tT = trialTypes
         plot([optoStimOnset optoStimOffset],[maxY{3}*.9 maxY{3}*.9],'Color',[1. 0.3333   0],'LineWidth',2,'LineStyle','-');
     end
     end
-    ax=gca;ax.XLim=[timeD(1) timeD(end)];
-    
+    ax=gca;ax.XLim=[timeD(1) timeD(end)];    
     ax.YLim =[minY{3} maxY{3}+.1];
 
-    subplot(6,length(trialTypes),matIP(4,ip))
+    subplot(6,length(varType),matIP(4,ip))
+    
+    
     imagesc(newTime,trialsNb,blocksAlligned,[p10{1} p90{1}+.1]);hold on
     pos=get(gca,'pos');
     c=colorbar('location','eastoutside',...
@@ -176,8 +241,8 @@ for tT = trialTypes
     end
     ax=gca; ax.XLim=[timeD(1) timeD(end)];
     
-    subplot(6,length(trialTypes),matIP(5,ip))
-    imagesc(timeV,trialsNb,abs(tempWheel),[p10{2} p90{2}+.1]);hold on
+    subplot(6,length(varType),matIP(5,ip))
+    imagesc(timeV,trialsNb,abs(blocksWheel),[p10{2} p90{2}+.1]);hold on
     pos=get(gca,'pos');
     c=colorbar('location','eastoutside',...
         'position',[pos(1)+pos(3)*1.01  pos(2) 0.01 pos(4)]);       
@@ -194,8 +259,8 @@ for tT = trialTypes
     end
     ax=gca; ax.XLim=[timeD(1) timeD(end)];
     
-    subplot(6,length(trialTypes),matIP(6,ip))
-    imagesc(Analysis.AllData.Licks.Bin{1}(2:end) ,trialsNb,tempLick,[p10{3}  max(p90{3},1)]);hold on
+    subplot(6,length(varType),matIP(6,ip))
+    imagesc(Analysis.AllData.Licks.Bin{1}(2:end) ,trialsNb,blocksLick,[p10{3}  max(p90{3},1)]);hold on
     pos=get(gca,'pos');
     c=colorbar('location','eastoutside',...
         'position',[pos(1)+pos(3)*1.01  pos(2) 0.01 pos(4)]);    
@@ -212,10 +277,21 @@ for tT = trialTypes
        ylabel('Trial Nb')
     end
     ax=gca; ax.XLim=[timeD(1) timeD(end)];
-    ip=ip+1;    
+    
+    ip=ip+1;        
+    
+    end
+    
+
+    
+   %
+    saveas(gcf,[Analysis.Properties.DirFig '\' Analysis.Properties.Name '_' Analysis.Properties.StateToZero{stateNb} '_' 'summary_variableTime' '.png']);
+    saveas(gcf,[Analysis.Properties.DirFig '\'  Analysis.Properties.Name '_' Analysis.Properties.StateToZero{stateNb} '_' 'summary_variableTime' '.m']);
+
 end
-saveas(gcf,[Analysis.Properties.DirFig '\' Analysis.Properties.Name '_' Analysis.Properties.StateToZero{stateNb} '_' 'summary_all' '.png']);
-saveas(gcf,[Analysis.Properties.DirFig '\' Analysis.Properties.Name '_' Analysis.Properties.StateToZero{stateNb} '_' 'summary_all' '.m']);
-end
+
+
+
+
 end
  
